73f6777829b3b2fa44234cd0a2e16cd1d0b28971
Cedric Sumner <cedricsumner@gmail.com>
2023-11-21 10:39:57 -0500

    Add dom manipulation logic to new code
    Yeah so I didn't like the way my new code was set up, so
    I made yet ANOTHER file. This one is set up for webpack, and
    the modules actually do the thing. Added the src and dist folders,
    with config files. The forecast divs know when you click on them and
    they'll soon be able to populate the hourly display with their data.

---
+++ src/DOM.js
@@ -0,0 +1,64 @@
+import { getTheWeather } from ".";
+
+export async function updateScreen(){
+    const test = await getTheWeather('macon');
+
+    const currentPack = test[0];
+    const forecastPack = test[1];
+
+    const currentTempDOM = document.querySelector('.current-temp');
+    const conditionIconDOM = document.querySelector('.condition-icon');
+    const conditionTextDOM = document.querySelector('.condition-text');
+    const currentFeelsDOM = document.querySelector('.current-feelslike');
+    
+    const highLowDOM = document.querySelector('.high-low');
+    const precipDOM = document.querySelector('.precip');
+    const humidityDOM = document.querySelector('.humidity');
+    const windDOM = document.querySelector('.wind');
+    const cloudDOM = document.querySelector('.cloud');
+    const uvDOM = document.querySelector('.uv-index');
+    const sunriseDOM = document.querySelector('.sunrise');
+    const sunsetDOM = document.querySelector('.sunset');
+    const moonphaseDOM = document.querySelector('.moonphase');
+    
+    const day0DOM = document.querySelector('.day0');
+    const day1DOM = document.querySelector('.day1');
+    const day2DOM = document.querySelector('.day2');
+    
+    const hourlyMenuDOM = document.querySelector('.hourly-scroll')
+
+    currentTempDOM.textContent = 'Temperature:' + currentPack[0];
+    conditionTextDOM.textContent = 'Condition: ' + currentPack[5];
+    currentFeelsDOM.textContent = 'Feels like: ' + currentPack[2];
+    highLowDOM.textContent = 'Max: ' + currentPack[16] +'/ Min: ' +currentPack[17];
+    precipDOM.textContent ='Precipitation:'+ currentPack[12];
+    humidityDOM.textContent ='Humidity:' + currentPack[4];
+    windDOM.textContent ='Wind:' + currentPack[9];
+    cloudDOM.textContent = 'Cloud cover: ' + currentPack[11];
+    uvDOM.textContent = 'UV Index: ' +currentPack[14];
+    sunriseDOM.textContent = 'Sunrise: ' + currentPack[7];
+    sunsetDOM.textContent = 'Sunset: ' + currentPack[8];
+    moonphaseDOM.textContent = 'Moon phase: ' + currentPack[15];
+
+    function displayForecast(){
+        let index=0;
+        for(let day of forecastPack){
+            const dayDom = document.querySelector(`.day${index}`);
+            const myid=index;
+            const date = document.createElement('div');
+            date.textContent = day[0];
+            const maxTemp = document.createElement('div');
+            maxTemp.textContent = day[1];
+            const minTemp = document.createElement('div');
+            minTemp.textContent = day[3];
+            const condition = document.createElement('div');
+            condition.textContent = day[5];
+            dayDom.append(date,maxTemp,minTemp,condition)
+            dayDom.addEventListener('click',()=>{
+                console.log(myid)
+            })
+            index++
+        }
+    }
+    displayForecast();
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-async function getTheWeather(query){
+import { updateScreen } from "./DOM";
+export async function getTheWeather(query){
     const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=7c5ae783336f49c7a99165328231411&q=${query}&days=3`,{mode: 'cors'});
     const json = await response.json();
 
@@ -18,6 +19,10 @@ async function getTheWeather(query){
     const tempInCel = current.temp_c;
     const percievedFar = current.feelslike_f;
     const percievedCel = current.feelslike_c;
+    const maxTempF = forecastDays[0].day.maxtemp_f;
+    const minTempF = forecastDays[0].day.mintemp_f;
+    const maxTempC = forecastDays[0].day.maxtemp_c;
+    const minTempC = forecastDays[0].day.mintemp_c;
     const humidity = current.humidity;
     const conditionText = current.condition.text;
     const conditionCode = current.condition.code;
@@ -26,20 +31,24 @@ async function getTheWeather(query){
     const windF = current.wind_mph;
     const windC = current.wind_kph;
     const cloud = current.cloud;
-    const rainChance = forecastDays[0].chance_of_rain;
-    const dayOrNo = current.isday;
+    const rainChance = forecastDays[0].day.daily_chance_of_rain;
+    const moonphase = forecastDays[0].astro.moon_phase;
+    const dayOrNo = current.is_day;
     const uv = current.uv;
 
+    const biggerArray=[];
     const currentArray=[];
+    const forecastArray =[];
 
     function getCurrent (){
-        currentArray.push(tempInFar,tempInCel,percievedFar,percievedCel,humidity,conditionText,conditionCode,sunrise,sunset,windF,windC,cloud,rainChance,dayOrNo,uv)
+        currentArray.length=0
+        currentArray.push(tempInFar,tempInCel,percievedFar,percievedCel,humidity,conditionText,conditionCode,sunrise,sunset,windF,windC,cloud,rainChance,dayOrNo,uv,moonphase,maxTempF,minTempF,maxTempC,minTempC)
         return currentArray;
     }
-
+    
     function daily(){
         let dayCount=0;
-        const forecastArray =[];
+        forecastArray.length=0;
         for(let day of forecastDays){
         const dayArray=[];
         const hourHolder=[];
@@ -71,7 +80,7 @@ async function getTheWeather(query){
             hourArray.push(hourlyTempF,hourlyTempC,hourlyFeelsF,hourlyFeelsC,hourlyConText,hourlyConCode,hourlyRainChance,hourlyHumidity,hourlyCloud)
             hourHolder.push(hourArray)
             }
-            dayArray.push(forecastMaxF,forecastMaxC,forecastMinF,forecastMinC,forecastConText,forecastConCode,hourHolder);
+            dayArray.push(dayDate,forecastMaxF,forecastMaxC,forecastMinF,forecastMinC,forecastConText,forecastConCode,hourHolder);
         }
         forecastArray.push(dayArray)
         hourly();
@@ -80,5 +89,10 @@ async function getTheWeather(query){
     }
     
     daily();
+    getCurrent();
+    biggerArray.length=0;
+    biggerArray.push(currentArray,forecastArray)
+    return biggerArray
 }
-getTheWeather('macon');
+updateScreen();
+
